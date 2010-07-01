ebcca55dc83c8db8745dfa1e241992b56b3b0bfe
LightStyle <damnednickix@hotmail.it>
2010-07-01 17:29:14 +0000

    Reso il codice di start dei bot molto semplice e pulito

--- __init__.py
+++ __init__.py
@@ -1,24 +1,5 @@
-from rss import *
-from trivia import *
-import Queue
+from conf import *
 
-channel = '#ff'
-input = Queue.Queue()
-output = Queue.Queue()
-irc = {}
-irc['rss'] = {}
-irc['trivia'] = {}
-irc['rss']['main'] = RssBot(channel, input, output)
-irc['trivia']['main'] = TriviaBot(channel, input, output)
-server = 'irc.azzurra.org'
-port = 6667
-irc['rss']['nickname'] = 'FF[RSS]'
-irc['trivia']['nickname'] = 'FF[TRIVIA]'
-irc['rss']['password'] = ''
-irc['trivia']['password'] = ''
-irc['rss']['ident'] = 'RSS'
-irc['trivia']['ident'] = 'TRIVIA'
-irc['rss']['main'].connect(server, port, irc['rss']['nickname'], irc['rss']['password'], irc['rss']['ident'])
-irc['trivia']['main'].connect(server, port, irc['trivia']['nickname'], irc['trivia']['password'], irc['trivia']['ident'])
-irc['rss']['main'].run()
-irc['trivia']['main'].run()
+for bot in irc:
+    irc[bot]['main'].connect(server, port, irc[bot]['nickname'], irc[bot]['password'], irc[bot]['ident'])
+    irc[bot]['main'].run()
